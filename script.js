220c1554e1cbc153223a3a76fcda30d796a6fe24
Danil Sadovoy <53143990+Sadovoy-Danil@users.noreply.github.com>
2019-07-25 19:13:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// "Comment me"!
+
+/* "And"
+       "ME!" */
+
+var border = 10; // var/let [название переменной] = [значение переменной];
+let second = 2;
+const pi = 3.14;
+
+// var number = 5; — число
+// var string = "Hello!"; — строка
+// var sym = Symbol(); — символ
+// var boolean = true; — булиновое значение
+// null;
+// undefined;
+// var obj = {}; — объект
